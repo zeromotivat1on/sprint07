f54f28d799ccd5c5eb870e0969859b54cfb2d8d2
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-03-31 11:17:51 +0300

    t00

---
+++ t00/index.php
@@ -0,0 +1,47 @@
+<?php
+    
+    session_start();
+
+    if (empty($_SESSION['counter']) || $_SESSION['counter'] >= 7 || !$_COOKIE['refreshCount']) {
+
+	    $_SESSION['counter'] = 1;
+        setcookie("refreshCount", $_SESSION['counter'], time() + 60);
+
+    } else {
+	        
+        $_SESSION['counter']++;
+        setcookie("refreshCount", $_SESSION['counter'], time() + 60);
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Cookie counter</h1>
+
+    <p>
+        This page was loaded 
+        <span>
+            <?php 
+
+                if(!$_COOKIE['refreshCount']) { echo 1; } else { echo $_COOKIE['refreshCount']; } 
+
+            ?>
+        </span> 
+        time(s) in last minute
+    </p>
+
+</body>
+
+</html>
